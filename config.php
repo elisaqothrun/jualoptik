0dec73c36c3b2b3a5876c18c61cbd9b00cca1f34
thisisalfarissitorus <sitorus64bit@gmail.com>
2019-12-15 19:58:20 +0700

    test
    test

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php 
+$koneksi = new mysqli("localhost", "root","","jualoptik");
+
+function tambahbarang(){
+	global $koneksi;
+}
+?>
--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,5 @@
-<?php $koneksi = new mysqli("localhost", "root","","jualoptik"); ?>
+<?php $koneksi = new mysqli("localhost", "root","alvadebian","jualoptik"); 
+if (!$koneksi){
+    die("Database tidak terkoneksi. " . mysqli_connect_error());
+}
+?>
